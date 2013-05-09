6d3c141f15e89d206aef5cc5cb6bc6cd6b763ce8
Kiks <edediego1@gmail.com>
2013-05-09 01:00:40 -0400

    Challenge answer

---
+++ Day5/challenges/integerSwapWithoutTemp.py
@@ -0,0 +1,20 @@
+'''
+Created on Apr 17, 2013
+
+@author: Kiks
+'''
+
+
+a = 3
+b = 7
+
+print("a:"+ str(a))
+print("b:"+str(b))
+
+a = a + b #a = 3 + 7  = 10
+b = a - b #b = 10 - 3 = 7
+a = a - b #a = 10 - 7 = 3
+
+print('')
+print("a:"+ str(a))
+print("b:"+str(b))
